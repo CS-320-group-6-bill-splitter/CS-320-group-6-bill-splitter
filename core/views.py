ac72472e67db0c5c8106414d37138255f4fcea50
Lucy Gavrich <gavrichlucy@gmail.com>
2026-03-04 14:48:53 -0500

    Actually add brainstorm

--- core/views.py
+++ core/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
 # endpoints
+
+# if not logged in, serve the login page
+
+# log in form
+
+# create account form
+
+# if logged in, serve the home page (list of households)
+
+# dashboard for specific household
+    # list of amounts owed by users
+    # list of bills for household
+
+# create household
+
+# add user to household
+
+# create bill form
+
+# view bill details
+
+# view amount details for specific user
+
+# user settings
+
+# logout
+
+# make payment form
