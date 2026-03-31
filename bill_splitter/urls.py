e5a8cef497c7315bb5be4e15d5cbde115517f0ab
Zoe Moccia <zoe.moccia@gmail.com>
2026-03-31 14:35:06 -0400

    Added login, register, logout views and corresponding url patterns

--- bill_splitter/urls.py
+++ bill_splitter/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,12 +1,49 @@
-from django.shortcuts import render
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+import json
+from .models import User
 
 # endpoints
 
 # if not logged in, serve the login page
 
-# log in form
+@require_POST
+def register(request):
+    data = json.loads(request.body)
+    email = data.get('email')
+    display_name = data.get('display_name')
+    password = data.get('password')
 
-# create account form
+    if User.objects.filter(email=email).exists():
+        return JsonResponse({'error': 'A user with this email already exists'}, status=400)
+
+    user = User.objects.create_user(email=email, display_name=display_name, password=password)
+    login(request, user)
+    return JsonResponse({'message': 'User created successfully',
+                         'display_name': user.display_name}, status=201)
+
+@require_POST
+def login_view(request):
+    data = json.loads(request.body)
+    email = data.get('email')
+    password = data.get('password')
+
+    user = authenticate(request, email=email, password=password)
+    if user is None:
+        return JsonResponse({'error': 'Invalid credentials'}, status=401)
+
+    login(request, user)
+    return JsonResponse({'message': 'Login successful', 'display_name': user.display_name, 'profile_picture': user.profile_picture,})
+
+@login_required
+@require_POST
+def logout_view(request):
+    logout(request)
+    return JsonResponse({'message': 'Logged out successfully'})
+
+# update user? display name, add/change profile image
 
 # if logged in, serve the home page (list of households)
 
@@ -25,7 +62,5 @@ from django.shortcuts import render
 # view amount details for specific user
 
 # user settings
-
-# logout
 
 # make payment form
