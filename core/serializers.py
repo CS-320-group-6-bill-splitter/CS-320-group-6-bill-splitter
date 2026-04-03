947f0df5c5ae85e4b5c81ba74e1e5b25739852c3
Zoe Moccia <zoe.moccia@gmail.com>
2026-04-03 10:33:56 -0400

    comments on what do add

--- core/serializers.py
+++ core/serializers.py
@@ -4,7 +4,8 @@ from .models import Household
 class HouseholdSerializer(serializers.ModelSerializer):
     members = serializers.StringRelatedField(many=True, read_only=True)
     member_count = serializers.IntegerField(source='members.count', read_only=True)
-
+    #total bills active for user
+    #total amount user owes for household
     class Meta:
         model = Household
         fields = ['id', 'name', 'members', 'member_count']
