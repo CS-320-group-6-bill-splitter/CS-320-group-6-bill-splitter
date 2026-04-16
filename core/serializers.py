d62bf36aac8d5f901a2c3e5127153ea8f88307ab
Lucy Gavrich <gavrichlucy@gmail.com>
2026-04-16 14:51:19 -0400

    Add BillDetailSerializer and PaymentSerializer

--- core/serializers.py
+++ core/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Household, User, Bill
+from .models import Household, User, Bill, Debt, Payment
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -45,3 +45,55 @@ class BillListSerializer(serializers.ModelSerializer):
         """Return a list of users who owe money for this bill."""
         users = [debt.user_owing for debt in bill.debts.all()]
         return UserSerializer(users, many=True).data
+
+
+class BillDetailSerializer(serializers.ModelSerializer):
+    """Serializer for the Bill model. For use in returning details of a bill."""
+    
+    debts = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Bill
+        fields = [
+            'id',
+            'name',
+            'amount',
+            'date_created',
+            'debts',
+        ]
+
+    def get_debts(self, bill):
+        """Return a list of debts for this bill."""
+        debts = bill.debts.all()
+        return DebtSerializer(debts, many=True).data
+    
+
+class PaymentSerializer(serializers.ModelSerializer):
+    """Serializer for the Payment model."""
+
+    class Meta:
+        model = Payment
+        fields = [
+            'id',
+            'amount',
+            'date_created',
+            'user_from',
+            'user_to',
+            'bill_name',
+        ]
+
+    def get_date_created(self, payment):
+        """Return the date the payment was created."""
+        return payment.date.isoformat()
+
+    def get_bill_name(self, payment):
+        """Return the name of the bill associated with this payment."""
+        return payment.bill.name
+    
+    def get_user_from(self, payment):
+        """Return the display name of the user making the payment."""
+        return payment.user_paying.display_name
+
+    def get_user_to(self, payment):
+        """Return the display name of the user receiving the payment."""
+        return payment.debt.bill.user_owed.display_name
